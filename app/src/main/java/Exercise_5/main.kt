53996549efe4fed85e682ee601f39cbeae9b1b2e
marcus_rk <makj0005@stud.kea.dk>
2024-02-15 16:12:28 +0100

    Done with word frequency exercise

---
+++ app/src/main/java/Exercise_5/main.kt
@@ -0,0 +1,16 @@
+package Exercise_5
+
+fun main() {
+    val words = listOf("apple", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple","banana", "apple", "orange", "banana","banana", "apple", "orange", "apple", "orange", "banana")
+    val frequencyMap: MutableMap<String, Int> = mutableMapOf()
+
+    // if a key exist in map, return the current value
+    // if a key does not exist, add it to map and set value to default 0
+    words.forEach {
+        val currentValue = frequencyMap.getOrDefault(it, 0)
+        frequencyMap[it] = currentValue + 1
+    }
+
+    // Print frequency map
+    println(frequencyMap)
+}
