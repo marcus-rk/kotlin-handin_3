770d659ee8f3a9d295961298a7457ca78561caf1
marcus_rk <makj0005@stud.kea.dk>
2024-02-21 17:11:10 +0100

    Exercise_5 now uses hashmap and arraylist

--- app/src/main/java/Exercise_5/main.kt
+++ app/src/main/java/Exercise_5/main.kt
@@ -1,8 +1,8 @@
 package Exercise_5
 
 fun main() {
-    val words = listOf("apple", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple","banana", "apple", "orange", "banana","banana", "apple", "orange", "apple", "orange", "banana")
-    val frequencyMap: MutableMap<String, Int> = mutableMapOf()
+    val words: List<String> = arrayListOf("apple", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple","banana", "apple", "orange", "banana","banana", "apple", "orange", "apple", "orange", "banana")
+    val frequencyMap: HashMap<String, Int> = HashMap()
 
     // if a key exist in map, return the current value
     // if a key does not exist, add it to map and set value to default 0
@@ -12,6 +12,6 @@ fun main() {
         frequencyMap[it] = currentValue + 1
     }
 
-    // Print frequency map
-    frequencyMap.forEach { (word, count) -> println("$word, count: $count") }
+    // Print frequency map (sorted)
+    frequencyMap.toSortedMap().forEach { (word, count) -> println("$word, count: $count") }
 }
